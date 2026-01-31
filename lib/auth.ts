e6a54b2c4999128e887d234db1929a7170148ed5
arash-jj <arshjfry66@gmail.com>
2026-01-31 15:24:53 +0330

    resolve  potential issue in auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,14 +2,16 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.DB_URI!)
-const db = client.db()
+const DB_URI = process.env.DB_URI;
+if (!DB_URI) {
+    throw new Error("Please define the DB_URI environment variable inside .env");
+}
+const client = new MongoClient(DB_URI);
+const db = client.db();
 
 export const auth = betterAuth({
-    database: mongodbAdapter(db,{
-        client,
-    }),
-    emailAndPassword: { 
-        enabled: true, 
-    },
+    secret: process.env.BETTER_AUTH_SECRET!,
+    baseURL: process.env.BETTER_AUTH_URL,
+    database: mongodbAdapter(db, { client }),
+    emailAndPassword: { enabled: true },
 });
